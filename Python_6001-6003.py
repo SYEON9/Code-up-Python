4ae7a888f03f47e32ecbee2955d3dd025f484065
Suyeon Kim <70511222+SYEON9@users.noreply.github.com>
2024-05-27 22:49:14 +0900

    Create Python_6001-6003.py

---
+++ Python_6001-6003.py
@@ -0,0 +1,20 @@
+# 기초-출력 
+# 출력하기01
+# print( )를 이용해 다음 단어를 출력하시오.
+# Hello
+print('Hello')
+
+
+# 기초-출력
+# 출력하기02
+# 이번에는 공백( )을 포함한 문장을 출력한다.
+# Hello World
+print('Hello World')
+
+
+# 기초-출력 
+# 출력하기03
+# 이번에는 줄을 바꿔 출력하는 출력문을 연습해보자.
+# Hello
+# World
+print('Hello\nWorld')
