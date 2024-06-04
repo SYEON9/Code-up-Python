10fb5f44572fd61f6f52ea0571b32af49c57d946
Suyeon Kim <70511222+SYEON9@users.noreply.github.com>
2024-06-04 23:43:31 +0900

    2024.06.04

---
+++ 1st_week/Python_6025-6027.py
@@ -0,0 +1,27 @@
+# 6025
+# 정수 2개를 입력받아
+# 합을 출력하는 프로그램을 작성해보자.
+a,b = input().split()
+a = int(a)
+b = int(b)
+print(a+b)
+
+
+# 6026
+# 실수 2개를 입력받아
+# 합을 출력하는 프로그램을 작성해보자.
+a,b = input().split()
+a = float(a)
+b = float(b)
+print(a+b)
+
+# 2개의 실수가 줄을 바꿔 입력된다.
+a = float(input())
+b = float(input())
+print(a+b)
+
+
+# 6027
+# 10진수를 입력받아 16진수(hexadecimal)로 출력해보자.
+a = int(input())
+print('%x'% a)
