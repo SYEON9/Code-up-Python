60b6ea2ef9159739057f2f043ef4d60ccfe815b7
Suyeon Kim <70511222+SYEON9@users.noreply.github.com>
2024-06-12 23:53:25 +0900

    2024.06.12

---
+++ 1st_week/Python_6049-6051.py
@@ -0,0 +1,22 @@
+# 6049
+# 두 정수(a, b)를 입력받아
+# a와 b의 값이 같으면 True 를, 같지 않으면 False 를 출력하는 프로그램을 작성해보자.
+a,b = map(int, input().split())
+if a==b: print('True')
+else: print('False')
+
+
+# 6050
+# 두 정수(a, b)를 입력받아
+# b의 값이 a의 값 보다 크거나 같으면 True 를, 같지 않으면 False 를 출력하는 프로그램을 작성해보자.
+a,b = map(int, input().split())
+if b>=a: print('True')
+else: print('False')
+
+
+# 6051
+# 두 정수(a, b)를 입력받아
+# a의 값이 b의 값과 서로 다르면 True 를, 같으면 False 를 출력하는 프로그램을 작성해보자.
+a,b = map(int, input().split())
+if a!=b: print('True')
+else: print('False')          
