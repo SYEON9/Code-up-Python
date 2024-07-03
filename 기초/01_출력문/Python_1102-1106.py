2a1864ade53cb7bb700352287fa82e5e59478ea0
Suyeon Kim <70511222+SYEON9@users.noreply.github.com>
2024-07-03 22:20:29 +0900

    2024.07.03

---
+++ 기초/01_출력문/Python_1102-1106.py
@@ -0,0 +1,12 @@
+# 1102
+print('Hello\nWorld!')
+
+
+# 1103
+print('"c:\\test"')
+
+
+# 1106
+int_min = -int('0b100000000000000000000000000000',2)
+int_max = int('0b011111111111111111111111111111',2)
+print(int_min, int_max)
