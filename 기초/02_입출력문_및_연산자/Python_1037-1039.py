203cb3e7684b9638328e30c306d399c863f813cf
Suyeon Kim <70511222+SYEON9@users.noreply.github.com>
2024-07-13 11:54:54 +0900

    2024.07.13

---
+++ 기초/02_입출력문_및_연산자/Python_1037-1039.py
@@ -0,0 +1,15 @@
+# 1037: 정수 입력받아 아스키 문자로 출력하기
+n = chr(int(input()))   # int -> 아스키 문자(chr)
+print(n)
+
+
+
+# 1038: 정수 2개 입력받아 합 출력하기1
+a,b = map(int, input().split())
+print(a+b)
+
+
+
+# 1039: 정수 2개 입력받아 합 출력하기2
+a,b = map(int, input().split())
+print(a+b)
