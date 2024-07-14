3b0c5a165c40282d20314c217ecd9059f2c09ffb
Suyeon Kim <70511222+SYEON9@users.noreply.github.com>
2024-07-14 15:22:25 +0900

    2024.07.14

---
+++ 기초/02_입출력문_및_연산자/Python_1040-1042.py
@@ -0,0 +1,14 @@
+# 1040: 정수 1개 입력받아 부호 바꿔 출력하기
+n = int(input())
+print(-n)
+
+
+# 1041: 문자 1개 입력받아 다음 문자 출력하기
+s= ord(input())
+s = chr(s+1)
+print(s)
+
+
+# 1042: 정수 1개 입력받아 나눈 몫 출력하기
+a,b = map(int, input().split())
+print(a//b)
