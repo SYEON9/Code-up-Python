267658e77302d9929b6d0cc6c9e6762c57abd23d
Suyeon Kim <70511222+SYEON9@users.noreply.github.com>
2024-07-15 22:31:42 +0900

    2024.07.15

---
+++ 기초/02_입출력문_및_연산자/Python_1043-1045.py
@@ -0,0 +1,18 @@
+# 1043: 정수 2개 입력받아 나눈 나머지 출력하기
+a,b = map(int, input().split())
+print(a%b)
+
+
+# 1044: 정수 1개 입력받아 1 더해 출력하기
+a = int(input())
+print(a+1)
+
+
+# 1045: 정수 2개 입력받아 자동 계산하기
+a,b = map(int, input().split())
+print(a+b)
+print(a-b)
+print(a*b)
+print(a//b)
+print(a%b)
+print(round(a/b,2))
