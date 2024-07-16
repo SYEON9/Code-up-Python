4a7a9a479ff286336e01692ce1d585a3fd94e40f
Suyeon Kim <70511222+SYEON9@users.noreply.github.com>
2024-07-17 00:01:43 +0900

    2024.07.16

---
+++ 기초/02_입출력문_및_연산자/Python_1046-1048.py
@@ -0,0 +1,14 @@
+# 1046: 정수 3개 입력받아 합과 평균 출력하기
+a,b,c = map(int, input().split())
+print(a+b+c)
+print((a+b+c)/3)
+
+
+# 1047: 정수 1개 입력받아 2배 곱해 출력하기
+a = int(input())
+print(a*2)
+
+
+# 1048: 한번에 2의 거듭제곱 배로 출력하기
+a,b = map(int, input().split())
+print(a*(2**b))
