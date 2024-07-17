018370858d56489b7eed1809d64e577fdf2d9144
Suyeon Kim <70511222+SYEON9@users.noreply.github.com>
2024-07-17 22:32:47 +0900

    2024.07.17

---
+++ 기초/02_입출력문_및_연산자/Python_1049-1051.py
@@ -0,0 +1,16 @@
+# 1049: 두 정수 입력받아 비교하기1
+a, b = map(int, input().split())
+if a>b: print(1)
+else: print(0)
+
+
+# 1050: 두 정수 입력받아 비교하기2
+a,b = map(int, input().split())
+if a!=b: print(1)
+else: print(0)
+
+
+# 1051: 두 정수 입력받아 비교하기3
+a,b=map(int, input().split())
+if b>=a: print(1)
+else: print(0)
