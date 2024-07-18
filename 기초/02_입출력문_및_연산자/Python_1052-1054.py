1e8864e090e71439dfcd54e76d7f86d5a09b12ec
Suyeon Kim <70511222+SYEON9@users.noreply.github.com>
2024-07-18 22:19:36 +0900

    2024.07.18

---
+++ 기초/02_입출력문_및_연산자/Python_1052-1054.py
@@ -0,0 +1,16 @@
+# 1052: 두 정수 입력받아 비교하기4
+a, b = map(int, input().split())
+if a!=b: print(1)
+else: print(0)
+
+
+# 1053: 참 거짓 바꾸기
+n = int(input())
+if n: print(0)
+else: print(1)
+
+
+# 1054: 둘 다 참일 경우만 참 출력하기
+a,b = map(int, input().split())
+if a+b==2: print(1)
+else: print(0)
