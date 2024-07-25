b03e6b00e8a06573c393b27236c9d7c012e6cd15
Suyeon Kim <70511222+SYEON9@users.noreply.github.com>
2024-07-25 23:47:46 +0900

    2024.07.25

---
+++ 기초/02_입출력문_및_연산자/Python_1110-1115.py
@@ -0,0 +1,28 @@
+# 1110: 정수 그대로 출력하기
+n = int(input())
+print(n)
+
+
+# 1111: %출력
+n = int(input())
+print(f'{n}%')
+
+
+# 1112: 두 정수 출력
+a,b = map(int, input().split())
+print(a,b)
+
+
+# 1113: 바꿔서 출력하기
+a,b = map(int, input().split())
+print(b,a)
+
+
+# 1114: 두 정수의 덧셈
+a,b = map(int, input().split())
+print(a+b)
+
+
+# 1115: 두 정수의 덧셈(64비트)
+a,b = map(int, input().split())
+print(a+b)
