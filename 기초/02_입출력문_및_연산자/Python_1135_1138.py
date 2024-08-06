5f0350ecdcd288756f32a69b8428e9db191964fa
Suyeon Kim <70511222+SYEON9@users.noreply.github.com>
2024-08-06 21:16:53 +0900

    2024.08.06

---
+++ 기초/02_입출력문_및_연산자/Python_1135_1138.py
@@ -0,0 +1,18 @@
+# 1135: 관계연산자1
+a,b = map(int, input().split())
+if a>=b: print(1)
+else: print(0)
+
+# 1136: 관계연산자2
+a,b = map(int, input().split())
+if a==b: print(1)
+else: print(0)
+
+# 1137: 관계연산자3
+a,b = map(int, input().split())
+if a==b: print(0)
+else: print(1)
+
+# 1138: 논리 연산자(NOT)
+a = int(input())
+print(!a)
